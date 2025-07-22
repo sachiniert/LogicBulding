b1ea391c0ec01a0ad09b40aa2bc973ebec91cf4d
sachiniert <sachin.yadav.sdet@gmail.com>
2025-07-22 18:07:01 +0530

    Using XOR method to find missing numbers

---
+++ src/AllLogicHere/MissingNumberInArray2MoreEffectiveWay.java
@@ -0,0 +1,20 @@
+package AllLogicHere;
+
+public class MissingNumberInArray2MoreEffectiveWay {
+
+	public static void main(String[] args) {
+		int numbers[]= {1,2,4,5,6};
+		int missingNumber =0;
+		
+		for(int no: numbers) {
+			missingNumber = missingNumber^no;
+		}
+
+		for(int i=0; i<=6; i++)
+		{
+			missingNumber = missingNumber ^ i;
+		}
+		System.out.println(missingNumber);
+	}
+
+}
