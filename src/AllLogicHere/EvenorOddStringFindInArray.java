b0e240cad5d78dc4e57ba338b262c527ee2d9951
sachiniert <sachin.yadav.sdet@gmail.com>
2025-07-22 17:18:03 +0530

    find even string in given string

---
+++ src/AllLogicHere/EvenorOddStringFindInArray.java
@@ -0,0 +1,24 @@
+package AllLogicHere;
+
+public class EvenorOddStringFindInArray {
+
+	public static void main(String[] args) {
+		//isme se hame even strng find karni hai
+		String input = "sky is blue and vast";
+		
+		// below line remove space and stroe string only in dataArray 
+		String dataArray[]= input.split(" ");
+		
+		for(String word : dataArray) {
+			if(word.length()%2 ==0) {
+				
+				System.out.println(word);
+			}else {
+				System.out.println(word);
+			}
+			
+		}
+		
+	}
+
+}
