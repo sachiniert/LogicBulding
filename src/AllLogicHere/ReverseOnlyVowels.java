2739b72c9464442188cc5a5aea30976af3e2cc57
sachiniert <sachin.yadav.sdet@gmail.com>
2025-07-24 19:26:58 +0530

    changes some in reversevowls

--- src/AllLogicHere/ReverseOnlyVowels.java
+++ src/AllLogicHere/ReverseOnlyVowels.java
@@ -6,17 +6,23 @@ public class ReverseOnlyVowels {
 		String input = "Hello";  //Holle
 		String vowelsString = "aeiouAEIOU";
 		
-		char inputArray[] = input.toCharArray();
-		int left =0;
-		int right = inputArray.length-1;
+		char inputArray[] = input.toCharArray();// convert string to array 
+		int left =0; // pointer 1 , 0 index
+		int right = inputArray.length-1; // pointer 2 , last index
 		
-		while(left < right) {
+		while(left < right) 
+		// 50 50 done by both pointer
+			{
 			
-			if(vowelsString.indexOf(inputArray[left])==-1) {
+			if(vowelsString.indexOf(inputArray[left])==-1)
+			//indexOf(inputArray[left] ->H vowels nhi to uska index -1
+			{
 				left++;
 				
 			}
-			else if(vowelsString.indexOf(inputArray[right])==-1) {
+			else if(vowelsString.indexOf(inputArray[right])==-1)
+			//right jisko point kar rha hai agar o vowel nhi 
+			{
 				right--;
 				
 			}else
@@ -30,7 +36,7 @@ public class ReverseOnlyVowels {
 				
 			}
 		}
-		System.out.println(input);
+	
 		System.out.println("Output "+new String(inputArray));
 
 	}
