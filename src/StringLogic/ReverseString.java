ab8e454bae80f77b6fe50dd84a0605559eda9eed
sachiniert <sachin.yadav.sdet@gmail.com>
2025-07-25 21:30:22 +0530

    Reverse String given String from user

---
+++ src/StringLogic/ReverseString.java
@@ -0,0 +1,23 @@
+package StringLogic;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		String rev;
+		
+		String rev2 ="";
+		System.out.println("Enter String: ");
+		Scanner sc = new Scanner(System.in);
+		rev=sc.nextLine();
+		
+		for(int i= rev.length()-1; i>=0; i--) {
+			rev2 = rev2+rev.charAt(i);
+		}
+		System.out.println(rev2);
+		
+
+	}
+
+}
